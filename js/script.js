78a7e2b7adeec6c26ff9701c66d4e4cb867623c7
evgeniy-she <schevchenko.ea@gmail.com>
2020-09-07 09:41:40 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+; (function () {
+	"use strict";
+	let btn = document.querySelector('.menu-toggle');
+	let menu = document.querySelector('.menu');
+	let hamRotate = document.querySelector('.hamRotate');
+	let ham1 = document.querySelector('.ham1');
+	btn.addEventListener('click', function (e) {
+		menu.classList.toggle('open');
+		hamRotate.classList.toggle('active');
+		ham1.classList.toggle('active');
+
+	});
+})();
+
